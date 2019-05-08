4fc915ac0ca4156adc1b8749963860f742308833
Joshua Weinstein <jweinstein@splunk.com>
2019-05-08 16:11:26 -0700

    traders module for cells

--- sapphire-cells/src/lib.rs
+++ sapphire-cells/src/lib.rs
@@ -1,7 +1,15 @@
-#[cfg(test)]
+
+/// This module contains cells which function as traders. 
+/// Traders are a type of cell which buy and sell in an item-based market. 
+/// As such, the mechanics these cells follow pertains to commodities and not 
+/// securities or other equity-based financial instruments.
+pub mod traders;
+
+
+/*#[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
-}
+}*/
